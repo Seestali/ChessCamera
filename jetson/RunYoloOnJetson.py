9cea5732edd5f2e157e09f78ac25222a8fbc75f9
Sebastian <seebasti@gmx.de>
2022-01-24 21:52:42 +0100

    deteted runs with small dataset

---
+++ jetson/RunYoloOnJetson.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+
+# git clone https://github.com/amirhosseinh77/JetsonYolo.git
+# configure camera
+# install requirements
+# get weights for inference
+# run JetsonYolo.py
