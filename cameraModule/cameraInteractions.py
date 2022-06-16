580e6dbadf8838a284b6c914bb435d6da446ab8f
Sebastian <seebasti@gmx.de>
2022-06-16 12:52:45 +0200

    Added function to take picture and open video stream

--- cameraModule/cameraInteractions.py
+++ cameraModule/cameraInteractions.py
@@ -1,17 +1,68 @@
-from picamery.array import PiRGBArray
-from picamera import PiCamera
-import time
 import cv2
 
-class Camera:
+def stream_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=1270,
+    display_height=720,
+    framerate=30,
+    flip_method=2,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d !"
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
 
+def show_camera():
+    window_title = "Check chessboard position"
 
+    print(stream_pipeline())
+    video_capture = cv2.VideoCapture(stream_pipeline, cv2.CAP_GSTREAMER)
+    if video_capture.isOpened():
+        try:
+            window_handle = cv2.namedWindow(window_title, cv2.WINDOW_AUTOSIZE)
+            while True:
+                ret_val, frame = video_capture.read()
+                # Check to see if the user closed the window
+                # Under GTK+ (Jetson Default), WND_PROP_VISIBLE does not work correctly. Under Qt it does
+                # GTK - Substitute WND_PROP_AUTOSIZE to detect if window has been closed by user
+                if cv2.getWindowProperty(window_title, cv2.WND_PROP_AUTOSIZE) >= 0:
+                    cv2.imshow(window_title, frame)
+                else:
+                    break
+                keyCode = cv2.waitKey(10) & 0xFF
+                # Stop the program on the ESC key or 'q'
+                if keyCode == 27 or keyCode == ord('q'):
+                    break
+        finally:
+            video_capture.release()
+            cv2.destroyAllWindows()
+    else:
+        print("Error: Unable to open camera")
 
-# initialize PiCamera and grab reference
-camera = PiCamera()
-rawCapture = PiRGBArray(camera)
+def captureImage():
+    cap = cv2.VideoCapture(0)
 
-time.sleep(0.1)
+    # Capture frame
+    ret, frame = cap.read()
+    if ret:
+        cv2.imwrite('home/chesscamera/projects/testimage.jpg', frame)
 
-camera.capture(rawCapture, format="rgb")
-image = rawCapture.array
+    cap.release()
+
+if __name__ == "__main__":
+    captureImage()
