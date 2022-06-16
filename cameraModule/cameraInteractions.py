b347089bb3d4beef5959f87e1b52d8436f86a43e
Sebastian <seebasti@gmx.de>
2022-06-16 13:15:32 +0200

    flipped

--- cameraModule/cameraInteractions.py
+++ cameraModule/cameraInteractions.py
@@ -14,7 +14,7 @@ def gstreamer_pipeline(
     display_width=960,
     display_height=540,
     framerate=30,
-    flip_method=2,
+    flip_method=0,
 ):
     return (
         "nvarguscamerasrc sensor-id=%d !"
@@ -48,8 +48,8 @@ def show_camera():
     window_title = "CSI Camera"
 
     # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
-    print(gstreamer_pipeline(flip_method=0))
-    video_capture = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+    print(gstreamer_pipeline(flip_method=2))
+    video_capture = cv2.VideoCapture(gstreamer_pipeline(flip_method=2), cv2.CAP_GSTREAMER)
     if video_capture.isOpened():
         try:
             window_handle = cv2.namedWindow(window_title, cv2.WINDOW_AUTOSIZE)
